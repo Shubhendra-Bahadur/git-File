76be22d9ed29100dda36b4cf21991c57230fef5e
Shubhendra Srivastava <shubhendrabahadur6@gmail.com>
2021-06-12 23:36:03 +0530

    sum modified

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum {
     public static void main(String[] args){
         int a=10;
         int b=20;
-        System.out.print(a+b);
+        System.out.print("message"+a+b);
     }
 }
